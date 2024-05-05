05d476b37f904a3cf8b03e8b3080ca264f66bbe4
EdwinRiv <dwein08@gmail.com>
2024-05-05 18:50:28 -0500

    Separando logica inicial de angular-bases en compononentes y modulos

--- 02-bases/src/app/app.component.ts
+++ 02-bases/src/app/app.component.ts
@@ -7,21 +7,4 @@ import { Component } from '@angular/core';
 })
 export class AppComponent {
   public title: string = 'Primera App de Angular';
-  public counter: number = 0;
-
-  public increment(): void {
-    this.counter++;
-  }
-
-  public decrement(): void {
-    this.counter--;
-  }
-
-  public increaseBy(value: number): void {
-    this.counter += value;
-  }
-
-  public resetCounter(): void {
-    this.counter = 0;
-  }
 }
--- 02-bases/src/app/counter/counter.component.ts
+++ 02-bases/src/app/counter/counter.component.ts
@@ -2,7 +2,14 @@ import { Component } from '@angular/core';
 
 @Component({
   selector: 'app-counter',
-  template: `<h1>Counter</h1>`,
+  template: `<h1>Counter: {{ counter }}</h1>
+    <button (click)="increment()" style="margin: 2px">Increment</button>
+    <button (click)="decrement()" style="margin: 2px">Decrement</button>
+    <button (click)="resetCounter()" style="margin: 2px">Reset Counter</button>
+    <br /><br />
+    <button (click)="increaseBy(5)">Inncrease By 5</button>
+    <br /><br />
+    <hr />`,
   styles: ['h1 { color: red; }'],
 })
 export class CounterComponent {
