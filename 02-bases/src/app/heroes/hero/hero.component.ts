788f505a4337bd6f028ddc2aa092135eca602e98
EdwinRiv <dwein08@gmail.com>
2024-05-05 20:55:30 -0500

    Asignando metodos public y getters al componente

--- 02-bases/src/app/heroes/hero/hero.component.ts
+++ 02-bases/src/app/heroes/hero/hero.component.ts
@@ -5,4 +5,43 @@ import { Component } from '@angular/core';
   templateUrl: './hero.component.html',
   styleUrl: './hero.component.css',
 })
-export class HeroComponent {}
+export class HeroComponent {
+  public heroName: string = 'Spider-Man';
+  public realName: string = 'Peter Parker';
+  public age: number = 24;
+  public canFly: boolean = false;
+  public superPowers: string[] = ['Super Strength', 'Web Slinging'];
+  public address: any = {
+    street: '123 Spider St',
+    city: 'New York',
+    state: 'NY',
+    zip: '10001',
+  };
+
+  // cuando se antepone de get, se convierte en una propiedad de solo lectura en el componente de Angular
+  get capitalizedName(): string {
+    return this.heroName.toUpperCase();
+  }
+
+  public getHeroDescription(): string {
+    return `${
+      this.heroName
+    } is a super hero with the following powers: ${this.superPowers.join(
+      ', '
+    )}`;
+  }
+
+  changeHero(): void {
+    this.heroName = 'Iron-Man';
+    this.realName = 'Tony Stark';
+    this.changeSuperPowers();
+  }
+
+  public changeAge(): void {
+    this.age = 45;
+  }
+
+  public changeSuperPowers(): void {
+    this.superPowers = ['Super Strength', 'Flight', 'Energy Blasts'];
+  }
+}
